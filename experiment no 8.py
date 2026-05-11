d14ed2f7988a2865f2aad86461f7cf6a6a485841
parikshit sandiprao chore <parikshitchore9@gmail.com>
2026-05-11 08:41:36 +0530

    Add files via upload

---
+++ experiment no 8.py
@@ -0,0 +1,65 @@
+my_dict = {
+    'apple': 50,
+    'banana': 20,
+    'cherry': 30,
+    'date': 10
+}
+
+parikshit={
+    'name':9,
+    'surname':5,
+    'age':19,
+    'class':13
+}
+#question no 1
+print(sorted(my_dict))
+
+#question no 2
+print('apple' in my_dict)
+
+#question no  3
+print(my_dict | parikshit)
+
+#question no 4
+my_tuple = (1, 2, 3)
+
+new_item=(4)
+
+nmy_tuple=my_tuple+new_item
+
+print(my_tuple)
+
+#question no 5
+tuple=( 3,'parikshit','true',3.7)
+
+#question no 6
+# List of numbers
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+
+print(total)
+
+#question no 7
+numbers = [10, 20, 30, 40, 50]
+
+print(max(numbers))
+
+#question no 8
+set={10,20,30,40}
+
+set.add(50)
+ 
+print(set)
+
+##question no 9
+my_list = [1, 2, 3, 4, 5]
+
+my_list.reverse()
+
+print(my_list)
+
+#question no 10
+my_list = [1, 2, 3, 4, 5]
+
+print(my_list[3])
